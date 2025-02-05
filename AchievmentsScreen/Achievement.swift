192b5887a38254fc3a71b982cd87ef5e26b2203c
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2025-02-05 19:34:01 +0300

    Added animation

---
+++ AchievmentsScreen/Achievement.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+struct Achievement {
+    let title: String
+    let description: String
+    let iconName: String
+    let color: UIColor
+    
+    
+}
--- AchievmentsScreen/ProfileViewController.swift
+++ AchievmentsScreen/ProfileViewController.swift
@@ -2,7 +2,8 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
-//    private var cardBottomConstra
+    private var cardBottomConstraint: NSLayoutConstraint!
+    private var isCardCollapsed: Bool = true
     
     private let cardView: UIView = {
         let view = UIView()
@@ -32,7 +33,7 @@ class ProfileViewController: UIViewController {
         label.font = .systemFont(ofSize: 14)
         label.textAlignment = .center
         label.numberOfLines = 1
-        label.alpha = UIConstants.collectionHiddenAlpha
+        label.alpha = 0
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
@@ -61,12 +62,15 @@ class ProfileViewController: UIViewController {
         cardView.addSubview(profileStackView)
         view.addSubview(cardView)
         
+        cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleCardTap)))
+        
         let nameLabelHeight = nameLabel.intrinsicContentSize.height
         let stackViewBottomPadding: CGFloat = 16
         let initialCardPosition = -(CGFloat(nameLabelHeight) + stackViewBottomPadding + view.safeAreaInsets.bottom)
+        cardBottomConstraint = cardView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: initialCardPosition)
         
         NSLayoutConstraint.activate([
-            cardView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: initialCardPosition),
+            cardBottomConstraint,
             cardView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: UIConstants.leadingInset),
             cardView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -UIConstants.leadingInset),
             cardView.heightAnchor.constraint(equalToConstant: UIConstants.cardViewHeight),
@@ -79,7 +83,25 @@ class ProfileViewController: UIViewController {
             avatarImageView.heightAnchor.constraint(equalToConstant: UIConstants.avatarImageSize)
             ])
     }
-
+    
+    @objc func handleCardTap() {
+        isCardCollapsed.toggle()
+        
+        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+            if self.isCardCollapsed {
+                let nameLabelHeight = self.nameLabel.intrinsicContentSize.height
+                let stackViewBottomPadding: CGFloat = 16
+                let targetPosition = -(CGFloat(nameLabelHeight) + stackViewBottomPadding + self.view.safeAreaInsets.bottom)
+                self.cardBottomConstraint.constant = targetPosition
+            } else {
+                self.cardBottomConstraint.constant = -(self.view.frame.height - self.view.safeAreaInsets.top - UIConstants.cardViewHeight)
+            }
+            self.bioLabel.alpha = self.isCardCollapsed ? 0 : 1
+            self.view.layoutIfNeeded()
+        })
+            
+        
+    }
 
 }
 
