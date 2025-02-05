5cb9e83703d73e0443e0d568035e0239bf8d4af1
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2025-02-05 18:25:41 +0300

    Deleted storyboard

---
+++ AchievmentsScreen/Constants.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+private enum UserInfo {
+    static let name = "Федор Конюхов"
+    static let bio = "Ну, наверное, в описании не нуждается"
+}
+
+private enum UIConstants {
+    static let cardCornerRadius: CGFloat = 32
+    static let cardShadowOpacity: Float = 0.1
+    static let cardShadowRadius: CGFloat = 8
+    static let cardShadowOffset: CGSize = CGSize(width: 0, height: -3)
+    
+    static let collectionItemSize: CGSize = CGSize(width: 150, height: 150)
+    static let collectionMinimumInteritemSpacing: CGFloat = 12
+    static let collectionMinimumLineSpacing: CGFloat = 16
+    static let collectionSectionInset: UIEdgeInsets = UIEdgeInsets(top: 12, left: 16, bottom: 16, right: 16)
+    
+    static let profileStackSpacing: CGFloat = 16
+    static let profileStackPadding: CGFloat = 24
+    static let avatarImageSize: CGFloat = 80
+    
+    static let cardDefaultBottomConstant: CGFloat = 50
+    static let collectionHiddenAlpha: CGFloat = 0
+}
