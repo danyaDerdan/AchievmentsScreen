06788a79be2016715bc60e1e55c9964236f499ea
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2025-02-05 18:14:34 +0300

    Initial Commit

---
+++ AchievmentsScreen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AchievmentsScreen
+//
+//  Created by Данил Толстиков on 05.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
