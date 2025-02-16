14d909d98937516f442c02656e0ba021b5d630c6
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2025-02-16 10:30:16 +0300

    Refactored

--- AchievmentsScreen/ProfileViewController.swift
+++ AchievmentsScreen/ProfileViewController.swift
@@ -1,12 +1,13 @@
 import UIKit
 
-class ProfileViewController: UIViewController{
+final class ProfileViewController: UIViewController {
     
     
-    private var cardBottomConstraint: NSLayoutConstraint!
-    private var collectionViewTopConstraint: NSLayoutConstraint!
+    private var cardBottomConstraint: NSLayoutConstraint?
+    private var collectionViewTopConstraint: NSLayoutConstraint?
     private var isCardCollapsed: Bool = true
     private var isDetailCardExpanded: Bool = false
+    private let detailView: DetailView? = DetailView()
     
     private let achievments = Achievement.demoData
     
@@ -78,6 +79,7 @@ class ProfileViewController: UIViewController{
         profileStackView.alignment = .center
         profileStackView.translatesAutoresizingMaskIntoConstraints = false
         
+        
         cardView.addSubview(profileStackView)
         view.addSubview(cardView)
         view.addSubview(collectionView)
@@ -89,6 +91,8 @@ class ProfileViewController: UIViewController{
         let initialCardPosition = -(CGFloat(nameLabelHeight) + stackViewBottomPadding + view.safeAreaInsets.bottom)
         cardBottomConstraint = cardView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: initialCardPosition)
         collectionViewTopConstraint = collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: UIConstants.cardViewHeight + UIConstants.leadingInset)
+        guard let cardBottomConstraint else { return }
+        guard let collectionViewTopConstraint else { return }
         
         NSLayoutConstraint.activate([
             cardBottomConstraint,
@@ -110,6 +114,7 @@ class ProfileViewController: UIViewController{
             ])
         
         cardView.layer.zPosition = 1
+        setupDetailView()
     }
     
     private func setupCollectionView() {
@@ -122,14 +127,16 @@ class ProfileViewController: UIViewController{
     @objc func handleCardTap() {
         isCardCollapsed.toggle()
         
-        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: {
+        UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseInOut, animations: { [weak self] in
+            guard let self else { return }
+            guard let cardBottomConstraint else { return }
             if self.isCardCollapsed {
                 let nameLabelHeight = self.nameLabel.intrinsicContentSize.height
                 let stackViewBottomPadding: CGFloat = 16
                 let targetPosition = -(CGFloat(nameLabelHeight) + stackViewBottomPadding + self.view.safeAreaInsets.bottom)
-                self.cardBottomConstraint.constant = targetPosition
+                self.cardBottomConstraint?.constant = targetPosition
             } else {
-                self.cardBottomConstraint.constant = -(self.view.frame.height - self.view.safeAreaInsets.top - UIConstants.cardViewHeight)
+                self.cardBottomConstraint?.constant = -(self.view.frame.height - self.view.safeAreaInsets.top - UIConstants.cardViewHeight)
             }
             self.collectionView.transform = self.isCardCollapsed ?
             CGAffineTransform(translationX: 0, y: self.view.frame.height / 2) :
@@ -150,72 +157,35 @@ class ProfileViewController: UIViewController{
     }
     @objc private func openCard(_ sender: UIPinchGestureRecognizer ) {
         if (!isDetailCardExpanded) {
-            showDetailView(achievment: achievments[sender.view?.tag ?? 0])
+            detailView?.configure(with: achievments[sender.view?.tag ?? 0])
+            detailView?.isHidden = false
             isDetailCardExpanded = true
         }
     }
     
     @objc private func closeCard(_ sender: UIPinchGestureRecognizer ) {
         if (isDetailCardExpanded) {
-            sender.view?.removeFromSuperview()
+            sender.view?.isHidden = true
             isDetailCardExpanded = false
         }
     }
     
-    private func showDetailView(achievment: Achievement) {
-        let view = UIView()
-        view.layer.cornerRadius = UIConstants.cardCornerRadius
-        view.backgroundColor = .white
-        view.addGestureRecognizer(UIPinchGestureRecognizer(target: self, action: #selector(closeCard)))
+    private func setupDetailView() {
+        guard let detailView else { return }
+        detailView.isHidden = true
+        view.addSubview(detailView)
+        detailView.layer.cornerRadius = UIConstants.cardCornerRadius
+        detailView.backgroundColor = .white
+        detailView.addGestureRecognizer(UIPinchGestureRecognizer(target: self, action: #selector(closeCard)))
         
-        let titleLabel = createLabel(text: achievment.title, font: .boldSystemFont(ofSize: 24))
-        let descriptionLabel = createLabel(text: achievment.description, font: .systemFont(ofSize: 18))
-        let dateLabel = createLabel(text: achievment.dateOfCompletion, font: .systemFont(ofSize: 14))
-        dateLabel.textColor = achievment.color
-        let imageView = UIImageView(image: UIImage(systemName: achievment.iconName))
-        imageView.contentMode = .scaleAspectFill
-        imageView.tintColor = achievment.color
-        let slider = UISlider()
-        slider.isEnabled = false
-        slider.minimumValue = 0
-        slider.maximumValue = 100
-        slider.setThumbImage(nil, for: .normal)
-        slider.minimumTrackTintColor = achievment.color
-        slider.value = Float(achievment.percentage)
-        let stackView = UIStackView(arrangedSubviews: [imageView, titleLabel, descriptionLabel, dateLabel])
-        stackView.axis = .vertical
-        stackView.spacing = UIConstants.profileStackSpacing
-        stackView.alignment = .center
-        
-        self.view.addSubview(view)
-        view.translatesAutoresizingMaskIntoConstraints = false
+        detailView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            view.topAnchor.constraint(equalTo: cardView.bottomAnchor, constant: UIConstants.leadingInset),
-            view.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -UIConstants.leadingInset),
-            view.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            view.widthAnchor.constraint(equalTo: cardView.widthAnchor)
+            detailView.topAnchor.constraint(equalTo: cardView.bottomAnchor, constant: UIConstants.leadingInset),
+            detailView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -UIConstants.leadingInset),
+            detailView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            detailView.widthAnchor.constraint(equalTo: cardView.widthAnchor)
         ])
-        
-        view.addSubview(stackView)
-        slider.translatesAutoresizingMaskIntoConstraints = false
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: UIConstants.leadingInset),
-            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -UIConstants.leadingInset),
-            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            imageView.heightAnchor.constraint(equalToConstant: UIConstants.avatarImageSize),
-            
-            ])
-        if (achievment.percentage != 100) {
-            view.addSubview(slider)
-            NSLayoutConstraint.activate([
-                slider.heightAnchor.constraint(equalToConstant: UIConstants.sliderHeight),
-                slider.widthAnchor.constraint(equalTo: stackView.widthAnchor, multiplier: UIConstants.widthMultiplier),
-                slider.centerXAnchor.constraint(equalTo: stackView.centerXAnchor),
-                slider.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: UIConstants.profileStackPadding)
-                ])
-        }
+        detailView.setup()
     }
     
     private func createLabel(text: String?, font: UIFont) -> UILabel {
